8a8e87ac5a1decbacd6703cf4799f0b34a0c7c32
Christian <destructordemundos3@outlook.com>
2024-09-12 22:31:07 -0500

    commit3

--- dist/controllers/auth.js
+++ dist/controllers/auth.js
@@ -14,32 +14,33 @@ const login = async (req, res) => {
     const userFound = await auth_1.default.findOne({ email });
     if (!userFound) {
         // "Este usuario no existe."
-        res.status(400).json({ message: "Contraseña incorrecta." });
+        res.status(400).json({ message: 'Contraseña incorrecta.' });
         return;
     }
     const isMatch = await (0, bcrypt_1.compare)(password, userFound.password);
     if (!isMatch) {
-        res.status(400).json({ message: "Contraseña incorrecta." });
+        res.status(400).json({ message: 'Contraseña incorrecta.' });
         return;
     }
     const generateToken = async (payload) => {
-        return jsonwebtoken_1.default.sign(payload, config_1.SECRET_KEY, { expiresIn: "16d" });
+        return jsonwebtoken_1.default.sign(payload, config_1.SECRET_KEY, { expiresIn: '16d' });
     };
     const token = await generateToken({ userId: userFound._id });
-    res.header("Authorization", `Bearer ${token}`);
-    res.cookie("auth_token", token, {
+    res.header('Authorization', `Bearer ${token}`);
+    res.cookie('auth_token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true,
+        sameSite: 'strict',
     });
-    res.status(200).json({ message: "Sesion iniciada" });
+    // process.env.NODE_ENV === "production"
+    res.status(200).json({ message: 'Sesion iniciada' });
 };
 exports.login = login;
 const register = async (req, res) => {
     const { email, password, name } = req.body;
     const userFound = await auth_1.default.findOne({ email });
     if (userFound) {
-        res.status(400).json({ message: "Esta cuenta ya existe." });
+        res.status(400).json({ message: 'Esta cuenta ya existe.' });
         return;
     }
     const passwordHash = await (0, bcrypt_1.hash)(password, 0);
@@ -50,22 +51,23 @@ const register = async (req, res) => {
     });
     const userSaved = await newUser.save();
     const generateToken = async (payload) => {
-        return jsonwebtoken_1.default.sign(payload, config_1.SECRET_KEY, { expiresIn: "16d" });
+        return jsonwebtoken_1.default.sign(payload, config_1.SECRET_KEY, { expiresIn: '16d' });
     };
     const token = await generateToken({ userId: userSaved._id });
-    console.log("token:", token);
-    res.cookie("auth_token", token, {
+    console.log('token:', token);
+    res.cookie('auth_token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true,
+        sameSite: 'strict',
     });
-    res.status(200).json({ message: "Cuenta creada" });
+    // process.env.NODE_ENV === "production"
+    res.status(200).json({ message: 'Cuenta creada' });
 };
 exports.register = register;
 const verifyAccessToken = async (req, res) => {
-    const userToken = req.cookies["auth_token"];
+    const userToken = req.cookies['auth_token'];
     if (!userToken) {
-        res.status(401).json({ message: "Token no encontrado." });
+        res.status(401).json({ message: 'Token no encontrado.' });
         return;
     }
     jsonwebtoken_1.default.verify(userToken, config_1.SECRET_KEY, async (error, user) => {
@@ -73,7 +75,7 @@ const verifyAccessToken = async (req, res) => {
             return res.sendStatus(401);
         const userFound = await auth_1.default.findById(user.userId);
         if (!userFound)
-            return res.status(400).json({ message: "Usuario no encontrado" });
+            return res.status(400).json({ message: 'Usuario no encontrado' });
         return res.json({
             userId: userFound._id,
             name: userFound.name,
@@ -84,17 +86,18 @@ const verifyAccessToken = async (req, res) => {
 exports.verifyAccessToken = verifyAccessToken;
 const logout = async (req, res) => {
     try {
-        res.cookie("auth_token", "", {
+        res.cookie('auth_token', '', {
             httpOnly: true,
             secure: true,
-            sameSite: "strict",
+            sameSite: 'strict',
             expires: new Date(0),
         });
-        res.status(200).json({ message: "Sesion cerrada" });
+        // process.env.NODE_ENV === "production"
+        res.status(200).json({ message: 'Sesion cerrada' });
     }
     catch (error) {
         if (error instanceof Error) {
-            res.status(500).json({ error: "Error interno del servidor" });
+            res.status(500).json({ error: 'Error interno del servidor' });
         }
     }
 };
@@ -103,18 +106,18 @@ const forgotPassword = async (req, res) => {
     const { email } = req.body;
     const userFound = await auth_1.default.findOne({ email });
     if (!userFound) {
-        res.status(400).json({ error: "Esta cuenta no existe." });
+        res.status(400).json({ error: 'Esta cuenta no existe.' });
         return;
     }
     const generateToken = async (payload) => {
-        return jsonwebtoken_1.default.sign(payload, config_1.SECRET_KEY, { expiresIn: "1h" });
+        return jsonwebtoken_1.default.sign(payload, config_1.SECRET_KEY, { expiresIn: '1h' });
     };
     const token = await generateToken({ userId: userFound._id });
     const resetLink = `http://localhost:5500/reset-password.html?token=${token}`;
     const msg = {
         to: email,
-        from: "destructordemundos3@outlook.com",
-        subject: "Restablecer contraseña",
+        from: 'destructordemundos3@outlook.com',
+        subject: 'Restablecer contraseña',
         html: `
       <h1>Restablece tu contraseña</h1>
       <p>Haz clic en el siguiente enlace para restablecer tu contraseña:</p>
@@ -124,39 +127,39 @@ const forgotPassword = async (req, res) => {
     };
     try {
         await mail_1.default.send(msg);
-        res.status(200).json({ message: "Enlace enviado" });
+        res.status(200).json({ message: 'Enlace enviado' });
     }
     catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Error al enviar el correo" });
+        res.status(500).json({ message: 'Error al enviar el correo' });
     }
 };
 exports.forgotPassword = forgotPassword;
 const resetPassword = async (req, res) => {
     const newPassword = req.body.newPassword;
-    const token = req.headers["authorization"]?.split(" ")[1];
+    const token = req.headers['authorization']?.split(' ')[1];
     try {
         const decoded = jsonwebtoken_1.default.verify(token, config_1.SECRET_KEY);
         const userFound = await auth_1.default.findById(decoded.userId);
         if (userFound !== null) {
             const isMatch = await (0, bcrypt_1.compare)(newPassword, userFound.password);
             if (isMatch) {
-                res.status(400).json({ error: "Esta contraseña ya esta en uso." });
+                res.status(400).json({ error: 'Esta contraseña ya esta en uso.' });
                 return;
             }
         }
         const passwordHash = await (0, bcrypt_1.hash)(newPassword, 0);
         try {
             await auth_1.default.findByIdAndUpdate(decoded.userId, { password: passwordHash }, { new: true });
-            res.status(200).json({ message: "Contraseña restablecida" });
+            res.status(200).json({ message: 'Contraseña restablecida' });
         }
         catch (error) {
-            res.status(500).json({ error: "Error" });
+            res.status(500).json({ error: 'Error' });
         }
     }
     catch (error) {
         console.log(error);
-        res.status(500).json({ error: "Error" });
+        res.status(500).json({ error: 'Error' });
     }
 };
 exports.resetPassword = resetPassword;
--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,10 +1,10 @@
-import type { Request, Response } from "express";
-import { compare, hash } from "bcrypt";
-import jwt from "jsonwebtoken";
-import sgMail from "@sendgrid/mail";
+import type { Request, Response } from 'express';
+import { compare, hash } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import sgMail from '@sendgrid/mail';
 
-import { SECRET_KEY } from "../config";
-import User from "../models/auth";
+import { SECRET_KEY } from '../config';
+import User from '../models/auth';
 
 const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -13,32 +13,34 @@ const login = async (req: Request, res: Response) => {
 
   if (!userFound) {
     // "Este usuario no existe."
-    res.status(400).json({ message: "Contraseña incorrecta." });
+    res.status(400).json({ message: 'Contraseña incorrecta.' });
     return;
   }
 
   const isMatch = await compare(password, userFound.password);
 
   if (!isMatch) {
-    res.status(400).json({ message: "Contraseña incorrecta." });
+    res.status(400).json({ message: 'Contraseña incorrecta.' });
     return;
   }
 
   const generateToken = async (payload: { userId: String }) => {
-    return jwt.sign(payload, SECRET_KEY, { expiresIn: "16d" });
+    return jwt.sign(payload, SECRET_KEY, { expiresIn: '16d' });
   };
 
   const token = await generateToken({ userId: userFound._id });
 
-  res.header("Authorization", `Bearer ${token}`);
+  res.header('Authorization', `Bearer ${token}`);
 
-  res.cookie("auth_token", token, {
+  res.cookie('auth_token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: 'strict',
   });
 
-  res.status(200).json({ message: "Sesion iniciada" });
+  // process.env.NODE_ENV === "production"
+
+  res.status(200).json({ message: 'Sesion iniciada' });
 };
 
 const register = async (req: Request, res: Response) => {
@@ -47,7 +49,7 @@ const register = async (req: Request, res: Response) => {
   const userFound = await User.findOne({ email });
 
   if (userFound) {
-    res.status(400).json({ message: "Esta cuenta ya existe." });
+    res.status(400).json({ message: 'Esta cuenta ya existe.' });
     return;
   }
 
@@ -62,30 +64,29 @@ const register = async (req: Request, res: Response) => {
   const userSaved = await newUser.save();
 
   const generateToken = async (payload: { userId: String }) => {
-    return jwt.sign(payload, SECRET_KEY, { expiresIn: "16d" });
+    return jwt.sign(payload, SECRET_KEY, { expiresIn: '16d' });
   };
 
   const token = await generateToken({ userId: userSaved._id });
 
-  console.log("token:", token);
+  console.log('token:', token);
 
-  res.cookie("auth_token", token, {
+  res.cookie('auth_token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: 'strict',
   });
 
-  res.status(200).json({ message: "Cuenta creada" });
+  // process.env.NODE_ENV === "production"
+
+  res.status(200).json({ message: 'Cuenta creada' });
 };
 
-const verifyAccessToken = async (
-  req: Request,
-  res: Response,
-): Promise<void> => {
-  const userToken = req.cookies["auth_token"];
+const verifyAccessToken = async (req: Request, res: Response): Promise<void> => {
+  const userToken = req.cookies['auth_token'];
 
   if (!userToken) {
-    res.status(401).json({ message: "Token no encontrado." });
+    res.status(401).json({ message: 'Token no encontrado.' });
     return;
   }
 
@@ -94,8 +95,7 @@ const verifyAccessToken = async (
 
     const userFound = await User.findById(user.userId);
 
-    if (!userFound)
-      return res.status(400).json({ message: "Usuario no encontrado" });
+    if (!userFound) return res.status(400).json({ message: 'Usuario no encontrado' });
 
     return res.json({
       userId: userFound._id,
@@ -107,17 +107,19 @@ const verifyAccessToken = async (
 
 const logout = async (req: Request, res: Response) => {
   try {
-    res.cookie("auth_token", "", {
+    res.cookie('auth_token', '', {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: 'strict',
       expires: new Date(0),
     });
 
-    res.status(200).json({ message: "Sesion cerrada" });
+    // process.env.NODE_ENV === "production"
+
+    res.status(200).json({ message: 'Sesion cerrada' });
   } catch (error) {
     if (error instanceof Error) {
-      res.status(500).json({ error: "Error interno del servidor" });
+      res.status(500).json({ error: 'Error interno del servidor' });
     }
   }
 };
@@ -128,12 +130,12 @@ const forgotPassword = async (req: Request, res: Response) => {
   const userFound = await User.findOne({ email });
 
   if (!userFound) {
-    res.status(400).json({ error: "Esta cuenta no existe." });
+    res.status(400).json({ error: 'Esta cuenta no existe.' });
     return;
   }
 
   const generateToken = async (payload: { userId: String }) => {
-    return jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+    return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
   };
 
   const token = await generateToken({ userId: userFound._id });
@@ -142,8 +144,8 @@ const forgotPassword = async (req: Request, res: Response) => {
 
   const msg = {
     to: email,
-    from: "destructordemundos3@outlook.com",
-    subject: "Restablecer contraseña",
+    from: 'destructordemundos3@outlook.com',
+    subject: 'Restablecer contraseña',
     html: `
       <h1>Restablece tu contraseña</h1>
       <p>Haz clic en el siguiente enlace para restablecer tu contraseña:</p>
@@ -154,16 +156,16 @@ const forgotPassword = async (req: Request, res: Response) => {
 
   try {
     await sgMail.send(msg);
-    res.status(200).json({ message: "Enlace enviado" });
+    res.status(200).json({ message: 'Enlace enviado' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Error al enviar el correo" });
+    res.status(500).json({ message: 'Error al enviar el correo' });
   }
 };
 
 const resetPassword = async (req: Request, res: Response) => {
   const newPassword = req.body.newPassword;
-  const token = req.headers["authorization"]?.split(" ")[1] as string;
+  const token = req.headers['authorization']?.split(' ')[1] as string;
 
   try {
     const decoded: any = jwt.verify(token, SECRET_KEY);
@@ -174,7 +176,7 @@ const resetPassword = async (req: Request, res: Response) => {
       const isMatch = await compare(newPassword, userFound.password);
 
       if (isMatch) {
-        res.status(400).json({ error: "Esta contraseña ya esta en uso." });
+        res.status(400).json({ error: 'Esta contraseña ya esta en uso.' });
         return;
       }
     }
@@ -182,26 +184,15 @@ const resetPassword = async (req: Request, res: Response) => {
     const passwordHash = await hash(newPassword, 0);
 
     try {
-      await User.findByIdAndUpdate(
-        decoded.userId,
-        { password: passwordHash },
-        { new: true },
-      );
-      res.status(200).json({ message: "Contraseña restablecida" });
+      await User.findByIdAndUpdate(decoded.userId, { password: passwordHash }, { new: true });
+      res.status(200).json({ message: 'Contraseña restablecida' });
     } catch (error) {
-      res.status(500).json({ error: "Error" });
+      res.status(500).json({ error: 'Error' });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Error" });
+    res.status(500).json({ error: 'Error' });
   }
 };
 
-export {
-  login,
-  register,
-  verifyAccessToken,
-  logout,
-  forgotPassword,
-  resetPassword,
-};
+export { login, register, verifyAccessToken, logout, forgotPassword, resetPassword };
