8a7b357dc04db7d34df6505cb942744374030b53
Christian <destructordemundos3@outlook.com>
2024-09-25 21:58:47 -0500

    commit 14

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,12 +1,11 @@
 import { connect } from "mongoose";
 import { DB_URL } from "../config.js";
 const connectDB = async () => {
-    try {
-        await connect(DB_URL);
-        console.log("Connected to DB");
-    }
-    catch (e) {
-        console.error(e);
-    }
+  try {
+    await connect(DB_URL);
+    console.log("Connected to DB");
+  } catch (e) {
+    console.error(e);
+  }
 };
 export default connectDB;
